35cad7c6b85efbeb59a13f7aee76c3c88f48cb15
Harpal Singh <harpalnitk@gmail.com>
2022-04-28 06:55:25 +0530

    work

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,3 +1,4 @@
+import { Lesson } from './../model/lesson';
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import { MatPaginator } from "@angular/material/paginator";
@@ -5,8 +6,8 @@ import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 import {Course} from "../model/course";
 import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay} from 'rxjs/operators';
-import {merge, fromEvent} from "rxjs";
+import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
+import {merge, fromEvent, throwError} from "rxjs";
 
 
 @Component({
@@ -17,86 +18,96 @@ import {merge, fromEvent} from "rxjs";
 export class CourseComponent implements OnInit, AfterViewInit {
 
     course:Course;
+    lessons: Lesson[];
+    loading=false;
+    expandedLesson:Lesson=null;
 
-    lessons = [
-       {
-        id: 120,
-        'description': 'Introduction to Angular Material',
-        'duration': '4:17',
-        'seqNo': 1,
-        courseId: 11
-      },
-      {
-        id: 121,
-        'description': 'Navigation and Containers',
-        'duration': '6:37',
-        'seqNo': 2,
-        courseId: 11
-      },
-      {
-        id: 122,
-        'description': 'Data Tables',
-        'duration': '8:03',
-        'seqNo': 3,
-        courseId: 11
-      },
-      {
-        id: 123,
-        'description': 'Dialogs and Overlays',
-        'duration': '11:46',
-        'seqNo': 4,
-        courseId: 11
-      },
-      {
-        id: 124,
-        'description': 'Commonly used Form Controls',
-        'duration': '7:17',
-        'seqNo': 5,
-        courseId: 11
-      },
-      {
-        id: 125,
-        'description': 'Drag and Drop',
-        'duration': '8:16',
-        'seqNo': 6,
-        courseId: 11
-      },
-      {
-        id: 126,
-        'description': 'Responsive Design',
-        'duration': '7:28',
-        'seqNo': 7,
-        courseId: 11
-      },
-      {
-        id: 127,
-        'description': 'Tree Component',
-        'duration': '11:09',
-        'seqNo': 8,
-        courseId: 11
-      },
-      {
-        id: 128,
-        'description': 'Virtual Scrolling',
-        'duration': '3:44',
-        'seqNo': 9,
-        courseId: 11
-      },
-      {
-        id: 129,
-        'description': 'Custom Themes',
-        'duration': '8:55',
-        'seqNo': 10,
-        courseId: 11
-      },
-      {
-        id: 130,
-        'description': 'Changing Theme at Runtime',
-        'duration': '12:37',
-        'seqNo': 11,
-        courseId: 11
-      }
-    ];
+    // lessons = [
+    //    {
+    //     id: 120,
+    //     'description': 'Introduction to Angular Material',
+    //     'duration': '4:17',
+    //     'seqNo': 1,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 121,
+    //     'description': 'Navigation and Containers',
+    //     'duration': '6:37',
+    //     'seqNo': 2,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 122,
+    //     'description': 'Data Tables',
+    //     'duration': '8:03',
+    //     'seqNo': 3,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 123,
+    //     'description': 'Dialogs and Overlays',
+    //     'duration': '11:46',
+    //     'seqNo': 4,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 124,
+    //     'description': 'Commonly used Form Controls',
+    //     'duration': '7:17',
+    //     'seqNo': 5,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 125,
+    //     'description': 'Drag and Drop',
+    //     'duration': '8:16',
+    //     'seqNo': 6,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 126,
+    //     'description': 'Responsive Design',
+    //     'duration': '7:28',
+    //     'seqNo': 7,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 127,
+    //     'description': 'Tree Component',
+    //     'duration': '11:09',
+    //     'seqNo': 8,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 128,
+    //     'description': 'Virtual Scrolling',
+    //     'duration': '3:44',
+    //     'seqNo': 9,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 129,
+    //     'description': 'Custom Themes',
+    //     'duration': '8:55',
+    //     'seqNo': 10,
+    //     courseId: 11
+    //   },
+    //   {
+    //     id: 130,
+    //     'description': 'Changing Theme at Runtime',
+    //     'duration': '12:37',
+    //     'seqNo': 11,
+    //     courseId: 11
+    //   }
+    // ];
+
+    displayedColumns =['seqNo','description','duration'];
+
+    @ViewChild(MatPaginator)
+    paginator: MatPaginator;
+    @ViewChild(MatSort)
+    sort:MatSort;
 
     constructor(private route: ActivatedRoute,
                 private coursesService: CoursesService) {
@@ -106,12 +117,44 @@ export class CourseComponent implements OnInit, AfterViewInit {
     ngOnInit() {
 
         this.course = this.route.snapshot.data["course"];
-
+        this.loadLessonsPage();
 
     }
+  loadLessonsPage() {
+    this.loading = true;
+    this.coursesService.findLessons(
+      this.course.id,
+      this.sort?.direction ?? 'asc',
+      this.paginator?.pageIndex ?? 0,
+      this.paginator?.pageSize ?? 3,
+      this.sort?.active ?? 'seqNo')
+    .pipe(
+      tap(lessons => this.lessons = lessons),
+      catchError(err => {
+        console.log('Error Loading Lessons', err);
+        alert('Error Loading Lessons');
+        return throwError(err);
+      }),
+      finalize(()=> this.loading = false)
+    )
+    .subscribe();
+  }
+  onToggleLesson(lesson:Lesson){
+    console.log('lesson',lesson);
+    console.log('expanded lesson',this.expandedLesson);
+  if(lesson == this.expandedLesson){
+    this.expandedLesson = null;
+  }else{
+    this.expandedLesson = lesson;
+  }
+   }
 
     ngAfterViewInit() {
-
+      this.sort.sortChange.subscribe(()=> this.paginator.pageIndex = 0);
+ merge(this.sort.sortChange,this.paginator.page)
+ .pipe(
+   tap(()=> this.loadLessonsPage())
+ ).subscribe();
 
     }
 
--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {FormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
-
+const SIMPLE_TEXT = "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ";
 @Component({
   selector: "create-course-step-1",
   templateUrl:"create-course-step-1.component.html",
@@ -15,12 +16,25 @@ export class CreateCourseStep1Component {
       Validators.minLength(5),
       Validators.maxLength(60)
     ]],
-    releasedAt: [new Date(), Validators.required],
+    releasedAt: [new Date(1990,3,1), Validators.required],
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SIMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
+
+  // Validators.requiredTrue makes checkboxes mandatory
+
+
+  //to highlight a particular Date in a calendar
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate,view)=>{
+    const date = cellDate.getDate();
+    if(view== 'month'){
+      return (date == 1) ? 'highlight-date second-css-class' : '';
+    }
+    //return value when we are not on monthly view
+    return "";
+  }
 
   constructor(private fb: FormBuilder) {
 
